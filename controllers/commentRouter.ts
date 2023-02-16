e35582c90385f1e689c248e87784384bafe84ee6
nashvador <nashvador@gmail.com>
2023-02-16 00:23:30 -0500

    Add depth first search to parse through comments

--- controllers/commentRouter.ts
+++ controllers/commentRouter.ts
@@ -24,7 +24,37 @@ commentRouter.post('/replyId/:id', async (request: GetUserAuthInfoRequest, respo
     response.json(newComment)
 })
 
+commentRouter.get('/postId/:id', async (request: GetUserAuthInfoRequest, response: Response) => {
+    const rootComments = await Comment.findAll({where: {postId: request.params.id, commentRespondToId: null}})
 
+    let originalQ = []
+
+    if (rootComments) {
+        for (let i=0; i <rootComments.length; i++) {
+            let queue:any = []
+            let commentStore = []
+            queue.push(rootComments[i])
+            while (queue.length > 0) {
+                let currentNode: any = queue[0]
+                const otherComment = await Comment.findAll({where : {commentRespondToId: currentNode.dataValues.id}})
+                commentStore.push(currentNode)
+                queue.shift()
+                if (otherComment) {
+                    queue = [...queue, ...otherComment]
+                } else {
+                    continue
+                }
+            }
+            originalQ.push(commentStore)
+        }
+    }
+    response.json(originalQ)
+})
+
+commentRouter.get("/:id", async (request: Request, response: Response) => {
+    const comment = await Comment.findByPk(request.params.id)
+    response.json(comment)
+})
 
 
 export {commentRouter}
