a0a61b1e0e1b58564d8012e441fb39fb34c80667
nashvador <nashvador@gmail.com>
2023-03-16 11:49:48 -0400

    Add sequelize count function

--- controllers/postRouter.ts
+++ controllers/postRouter.ts
@@ -1,72 +1,94 @@
 import { Router, Request, Response } from "express";
 import { User, Post, Comment } from "../models";
+import { sequelize } from "../util/db";
 import { GetUserAuthInfoRequest } from "../util/middleware";
-const postRouter = Router()
+const postRouter = Router();
 
 postRouter.get("/", async (_request: Request, response: Response) => {
-    const posts = await Post.findAll({ attributes: { exclude: ['userId'] },
-    include: {
-      model: User,
-      attributes: ['username']
-    },})
-    response.json(posts)
-})
+  const posts = await Post.findAll({
+    attributes: { exclude: ["userId"] },
+    group: ["post.id", "user.id", "comments.id"],
+    include: [
+      {
+        model: User,
+        attributes: ["username"],
+      },
+      {
+        model: Comment,
+        attributes: [
+          [sequelize.fn("COUNT", sequelize.col("comments.id")), "commentCount"],
+        ],
+      },
+    ],
+  });
+  response.json(posts);
+});
 
 postRouter.get("/:id", async (request: Request, response: Response) => {
-    const posts = await Post.findByPk(request.params.id, { attributes: { exclude: ['userId'] },
-    include: [{
+  const posts = await Post.findByPk(request.params.id, {
+    attributes: { exclude: ["userId"] },
+    include: [
+      {
         model: Comment,
-        include: [{
-          model: User,
-          attributes: ['username']
-        }]
-      }]
-})
-    response.json(posts)
-})
+        include: [
+          {
+            model: User,
+            attributes: ["username"],
+          },
+        ],
+      },
+    ],
+  });
+  response.json(posts);
+});
 
+postRouter.post(
+  "/",
+  async (request: GetUserAuthInfoRequest, response: Response) => {
+    const body = request.body;
+    const user = request.user;
 
-postRouter.post("/", async (request: GetUserAuthInfoRequest, response: Response) => {
-    const body = request.body
-    const user = request.user
+    const newPost = await Post.create({
+      postTitle: body.title,
+      upVotes: 0,
+      postContent: body.content,
+      userId: user?.dataValues.id,
+    });
 
+    response.json(newPost);
+  }
+);
 
-    const newPost = await Post.create({ postTitle: body.title,
-        upVotes: 0,
-        postContent: body.content,
-        userId: user?.dataValues.id,})
-
-    response.json(newPost)
-
-})
-
-postRouter.delete("/:id", async (request: GetUserAuthInfoRequest, response: Response) => {
-    const post : Post | null = await Post.findByPk(request.params.id)
-    const user = request.user
+postRouter.delete(
+  "/:id",
+  async (request: GetUserAuthInfoRequest, response: Response) => {
+    const post: Post | null = await Post.findByPk(request.params.id);
+    const user = request.user;
 
     if (user?.dataValues.admin) {
-        await post?.destroy();
-        response.status(404).end();
+      await post?.destroy();
+      response.status(404).end();
     }
     if (post?.userId === user?.dataValues.id) {
-        await post?.destroy()
-        response.status(404).end();
+      await post?.destroy();
+      response.status(404).end();
     }
-})
+  }
+);
 
-postRouter.put("/:id", async (request: GetUserAuthInfoRequest, response: Response) => {
-    const post : Post | null = await Post.findByPk(request.params.id)
-    const user = request.user
-    const {content} = request.body
+postRouter.put(
+  "/:id",
+  async (request: GetUserAuthInfoRequest, response: Response) => {
+    const post: Post | null = await Post.findByPk(request.params.id);
+    const user = request.user;
+    const { content } = request.body;
 
     if (post && post?.userId === user?.dataValues.id) {
-        post.postContent = content
-        await post.save()
-        response.json(post)
+      post.postContent = content;
+      await post.save();
+      response.json(post);
     }
+  }
+);
 
-
-})
-
-
-export {postRouter}
+export { postRouter };
