bd0e8d01b456d41f73db974d535755d097b24aee
nashvador <nashvador@gmail.com>
2023-03-20 11:36:55 -0400

    Fix formatting on login router page

--- controllers/loginRouter.ts
+++ controllers/loginRouter.ts
@@ -1,37 +1,34 @@
 import jwt from "jsonwebtoken";
 import { Response, Request, Router } from "express";
 import { User } from "../models";
-import bcrypt from "bcryptjs"
+import bcrypt from "bcryptjs";
 const loginRouter = Router();
 
-
-
 loginRouter.post("/", async (request: Request, response: Response) => {
-    const { username, password }: { username: string; password: string } =
+  const { username, password }: { username: string; password: string } =
     request.body;
 
-    const user : User | null = await User.findOne({where: {username: username} });
-    const passwordCorrect =
+  const user: User | null = await User.findOne({
+    where: { username: username },
+  });
+  const passwordCorrect =
     user === null ? false : bcrypt.compareSync(password, user.password);
 
-    if (!(user && passwordCorrect)) {
-        return response.status(401).json({
-          error: "Invalid username or password",
-        });
-      }
-    
-      const userForToken = {
-        username: user.username,
-        id: user.id,
-      };
-    
-      const token = jwt.sign(userForToken, process.env.SECRET!, {
-        expiresIn: 60 * 60,
-      });
-      response
-        .status(200)
-        .send({ token, username: user.username, name: user.name });
+  if (!(user && passwordCorrect)) {
+    return response.status(401).json({
+      error: "Invalid username or password",
+    });
+  }
 
-})
+  const userForToken = {
+    username: user.username,
+    id: user.id,
+  };
 
-export {loginRouter}
+  const token = jwt.sign(userForToken, process.env.SECRET!);
+  response
+    .status(200)
+    .send({ token, username: user.username, name: user.name });
+});
+
+export { loginRouter };
