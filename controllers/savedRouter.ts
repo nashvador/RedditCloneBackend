e39fc746f560372af25b6a019a1411ee576e1d27
nashvador <nashvador@gmail.com>
2023-04-05 14:10:42 -0400

    Add prettier formatting to SavedRouter

--- controllers/savedRouter.ts
+++ controllers/savedRouter.ts
@@ -1,45 +1,65 @@
 import { Router, Request, Response } from "express";
 import { User, Post, Comment, Saved } from "../models";
 import { GetUserAuthInfoRequest } from "../util/middleware";
-const savedRouter = Router()
+const savedRouter = Router();
 
-savedRouter.get("/", async (request: GetUserAuthInfoRequest, response: Response) => {
-    const userId = request.user?.dataValues.id
+savedRouter.get(
+  "/",
+  async (request: GetUserAuthInfoRequest, response: Response) => {
+    const userId = request.user?.dataValues.id;
 
-    const savedPostsAndComments = await Saved.findAll({where: {userId: userId}, order: [['createdAt','DESC']], include: [{
-        model: Comment, include: [{model: User}]
-    }, {model: Post, include: [{model: User}]}]})
-    response.json(savedPostsAndComments)
-})
+    const savedPostsAndComments = await Saved.findAll({
+      where: { userId: userId },
+      order: [["createdAt", "DESC"]],
+      include: [
+        {
+          model: Comment,
+          include: [{ model: User }],
+        },
+        { model: Post, include: [{ model: User }] },
+      ],
+    });
+    response.json(savedPostsAndComments);
+  }
+);
 
-savedRouter.post("/commentId/:id", async (request: GetUserAuthInfoRequest, response: Response) => {
-    const commentId = request.params.id
-    const userId = request.user?.dataValues.id
-    const savedComment = await Saved.create({userId: userId, commentId: commentId})
+savedRouter.post(
+  "/commentId/:id",
+  async (request: GetUserAuthInfoRequest, response: Response) => {
+    const commentId = request.params.id;
+    const userId = request.user?.dataValues.id;
+    const savedComment = await Saved.create({
+      userId: userId,
+      commentId: commentId,
+    });
 
-    response.json(savedComment)
-})
+    response.json(savedComment);
+  }
+);
 
+savedRouter.post(
+  "/postId/:id",
+  async (request: GetUserAuthInfoRequest, response: Response) => {
+    const postId = request.params.id;
+    const userId = request.user?.dataValues.id;
+    const savedComment = await Saved.create({ userId: userId, postId: postId });
 
-savedRouter.post("/postId/:id", async (request: GetUserAuthInfoRequest, response: Response) => {
-    const postId = request.params.id
-    const userId = request.user?.dataValues.id
-    const savedComment = await Saved.create({userId: userId, postId: postId})
+    response.json(savedComment);
+  }
+);
 
-    response.json(savedComment)
-})
-
-savedRouter.delete("/:id", async (request: GetUserAuthInfoRequest, response: Response) => {
-    const savedId = await Saved.findByPk(request.params.id)
-    const userId = request.user?.dataValues.id
+savedRouter.delete(
+  "/:id",
+  async (request: GetUserAuthInfoRequest, response: Response) => {
+    const savedId = await Saved.findByPk(request.params.id);
+    const userId = request.user?.dataValues.id;
 
     if (savedId?.userId === userId && savedId) {
-        await savedId.destroy()
-        response.status(404).end();
+      await savedId.destroy();
+      response.status(404).end();
     }
-    response.json(savedId)
-})
+    response.json(savedId);
+  }
+);
 
-
-
-export {savedRouter}
+export { savedRouter };
